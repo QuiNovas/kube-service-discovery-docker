19e927f473ac246f6571616ba1c42cc99e6ac481
Mathew Moon <mmoon@athena-iot.com>
2019-04-01 16:13:44 -0500

    Add env vars for http and https service ports

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-
 var sub = require('child_process');
 var poll = sub.fork('./kubepoll.js');
 var hostList = [];
-
 var httpPort = process.env.HTTP_PORT || 80
 var httpsPort = process.env.HTTPS_PORT || 443
-
-var servicePort = process.env.SERVICE_PORT || 443
+var httpServicePort = process.env.HTTP_SERVICE_PORT || 443
+var httpsServicePort = process.env.HTTPS_SERVICE_PORT || 443
 
 poll.on('message', function(m) {
   console.log("New hostlist received: " + m);
@@ -19,8 +17,8 @@ poll.on('message', function(m) {
 http.createServer(function(req, res){
   var host = hostList[Math.floor(Math.random()*hostList.length)];
   res.writeHead(200, {"Content-Type": "text/plain"});
-  if(servicePort != 443){
-    res.writeHead(302, 'https://' + host + ':' + servicePort + req.url);
+  if(httpServicePort != 80){
+    res.writeHead(302, 'https://' + host + ':' + httpServicePort + req.url);
   }else{
     res.writeHead(302, 'https://' + host + req.url);
   }
@@ -35,8 +33,8 @@ var ssl = {
 var httpsServer = https.createServer(ssl, function (req, res) {
   var host = hostList[Math.floor(Math.random()*hostList.length)];
   res.writeHead(200, {"Content-Type": "text/plain"});
-  if(servicePort != 443){
-    res.writeHead(302, 'https://' + host + ':' + servicePort + req.url);
+  if(httpsServicePort != 443){
+    res.writeHead(302, 'https://' + host + ':' + httpsServicePort + req.url);
   }else{
     res.writeHead(302, 'https://' + host + req.url);
   }
